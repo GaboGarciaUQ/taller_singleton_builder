3d4de794380faca46f84804672f7d8e620f7f743
GaboGarciaUQ <gabriel.garciad@uqvirtual.edu.co>
2026-09-17 21:09:31 -0500

    Ejercicio 2 - Builder Añadido

--- src/main/java/com/uniquindio/co/Compra.java
+++ src/main/java/com/uniquindio/co/Compra.java
@@ -5,28 +5,100 @@ import java.util.List;
 
 public class Compra {
 
-    private String cliente;
-    private String funcion;
-    private List<String> asientos;
-    private String combo;
-    private String promocion;
-    private int puntosredimidos;
-    private LocalDate fecha;
-    private boolean esCortesia;
+    private final String cliente;
+    private final String funcion;
+    private final List<String> asientos;
+    private final LocalDate fecha;
+    private final String combo;
+    private final String promocion;
+    private final int puntosredimidos;
+    private final  boolean esCortesia;
 
-    public Compra(String cliente, String funcion, List<String> asientos, String combo, String promocion, int puntosredimidos, LocalDate fecha, boolean esCortesia) {
+    private Compra(Builder b){
 
-        this.cliente = cliente; //Necesario
-        this.funcion = funcion; //Necesario
-        this.asientos = asientos; //Necesario
-        this.combo = combo;
-        this.promocion = promocion;
-        this.puntosredimidos = puntosredimidos;
-        this.fecha = fecha;
-        this.esCortesia = esCortesia;
+        this.cliente = b.cliente;
+        this.funcion = b.funcion;
+        this.asientos = b.asientos;
+        this.fecha = b.fecha;
+        this.combo = b.combo;
+        this.promocion = b.promocion;
+        this.puntosredimidos =b.puntosredimidos;
+        this.esCortesia = b.esCortesia;
     }
 
-    public Compra(){
+    public static class Builder{
+
+        private String cliente;
+        private String funcion;
+        private List<String> asientos;
+        private LocalDate fecha;
+        private String combo;
+        private String promocion;
+        private int puntosredimidos;
+        private boolean esCortesia;
+
+        public Builder conCliente(String c){
+            this.cliente = c;
+            return this; //al final de cada metodo builder se retorna el objeto/clase que estamos creando para que se guarden los datos dentro de la clase compra.
+        }
+
+        public Builder conFuncion(String f){
+            this.funcion = f;
+            return this;
+        }
+        
+        public Builder conAsientos(List<String> as){
+            this.asientos = as;
+            return this;
+        }
+
+        public Builder conFecha(LocalDate fe){
+            this.fecha= fe;
+            return this;
+        }
+
+        public Builder conCombo(String c){
+            this.combo = c;
+            return this;
+        }
+
+        public Builder conPromocion(String p){
+            this.promocion = p;
+            return this;
+        }
+
+        public Builder conPuntosRedimidos(int p){
+            this.puntosredimidos = p;
+            return this;
+        }
+
+        public Builder conEsCortesia(boolean c){
+            this.esCortesia = c;
+            return this;
+        }
+
+        public Compra build(){
+
+            if(cliente.equals(null)){
+                throw new IllegalStateException("Dato Obligatorio no encontrado. Porfavor inserte un Cliente");
+            }
+            if(funcion.equals(null)){
+                throw new IllegalStateException("Dato Obligatorio no encontrado. Porfavor inserte una funcion");
+            }
+            if(asientos.equals(null)){
+                throw new IllegalStateException("Dato Obligatorio no encontrado. Porfavor inserte uno o mas asientos");
+            }
+            if(asientos.size()>5){
+                throw new IllegalStateException("Ha excedido la maxima cantidad de asientos reservables por Usuario. Por favor reserve menos de 5 asientos.");
+            }
+            if(puntosredimidos!=0 && puntosredimidos<100){
+                throw new IllegalStateException("La cantidad de puntos redimidos es muy pequeña. Para obtener beneficios tiene que redimir mas de 100 puntos");
+            }
+            return new Compra(this);
+        }
+
+
+
 
     }
 
--- src/main/java/com/uniquindio/co/Main.java
+++ src/main/java/com/uniquindio/co/Main.java
@@ -1,8 +1,54 @@
 package com.uniquindio.co;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        Compra comprapeq = new Compra.Builder()
+            .conCliente("Juan")
+            .conFuncion("Spider-Man")
+            .conAsientos(new ArrayList<>(Arrays.asList("F4","F9","C1")))
+            .conFecha(LocalDate.now())
+            .build();
+
+        Compra compracomp = new Compra.Builder()
+            .conCliente("Mateo")
+            .conFuncion("La Odisea")
+            .conAsientos(new ArrayList<>(Arrays.asList("D1","D2")))
+            .conFecha(LocalDate.now())
+            .conCombo("Combo Perro Crispetas")
+            .conPromocion("Promocion Familiar")
+            .conPuntosRedimidos(120)
+            .conEsCortesia(false)
+            .build();
+
+        //VERIFICACIONES 
+
+        // Compra compra1 = new Compra(); // No permite pasar "The constructor Compra() is undefined"
+
+        // Compra compra2 = new Compra.Builder().build(); // No crea el objeto y lanza NullPointerException "Cannot invoke "String.equals(Object)" because "this.cliente" is null"
+
+        //VALIDACIONES (Para correr Main bien hay que quitar estas pues tiran IllegalStateException)
+
+        Compra compraval1 = new Compra.Builder()
+            .conCliente("Jose")
+            .conFuncion("La Odisea")
+            .conAsientos(new ArrayList<>(Arrays.asList("K4","K9","I5","I6","I7","B6")))
+            .conFecha(LocalDate.now())
+            .build();
+
+        Compra compraval2 = new Compra.Builder()
+            .conCliente("Oscar")
+            .conFuncion("Avengers: Doomsday")
+            .conAsientos(new ArrayList<>(Arrays.asList("H1")))
+            .conFecha(LocalDate.now())
+            .conCombo("Combo Nachos")
+            .conPuntosRedimidos(80)
+            .build();
         
     }
 }
